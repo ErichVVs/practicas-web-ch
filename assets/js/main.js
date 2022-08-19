5b82163108e7ee10306accbb49ca72fac6446f6e
erichvvs <erich.adan@gmail.com>
2022-08-18 19:38:06 -0500

    metiendo javascrpit a pupilas

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let pupilHTMLCollection = document.getElementsByClassName('pupil');
+let pupilArray = Array.from (pupilHTMLCollection);
+
+let input = {
+    mouseX:{
+        start: 0,
+        end: window.innerW
+    },
+    mousey:{
+    
+    }
+}
+
